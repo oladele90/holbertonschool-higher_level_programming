80b0b6ee5f318ba7b32476d35eb252b8c26af814
oladele90 <oladelemiro07@gmail.com>
2023-01-08 10:16:10 -0800

    last digit

---
+++ 0x00-python-hello_world/3-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+number2 = abs(number) % 10
+str = ""
+if number < 0:
+    number2 = -number2
+if number2 < 5:
+    str = ("less than 6 and not 0")
+elif number2 == 0:
+    str = ("0")
+else:
+    str = ("greater than 5")
+print("the last digit of {} is {} and is {} ".format(number, number2, str))
