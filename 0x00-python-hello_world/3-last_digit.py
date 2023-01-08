e6dfd37223be8a84c08d7f0381930788484dbced
oladele90 <oladelemiro07@gmail.com>
2023-01-08 10:21:14 -0800

    last digit

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -5,10 +5,10 @@ number2 = abs(number) % 10
 str = ""
 if number < 0:
     number2 = -number2
-if number2 < 5:
-    str = ("less than 6 and not 0")
-elif number2 == 0:
+if number2 == 0:
     str = ("0")
+elif number2 < 5:
+    str = ("less than 6 and not 0")
 else:
     str = ("greater than 5")
 print("Last digit of {} is {} and is {}".format(number, number2, str))
