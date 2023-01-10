791da1f1aee034629373094f81248f4960c8d298
oladele90 <oladelemiro07@gmail.com>
2023-01-10 10:30:57 -0800

    args

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-count = len(sys.argv) - 1
-if count < 1:
-    print("{} arguments.".format(count))
-elif count == 1:
-    print("{} argument:".format(count))
-else:
-    print("{} arguments:".format(count))
-for i in range(1, count + 1):
-    print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv) - 1
+    if count < 1:
+        print("{} arguments.".format(count))
+    elif count == 1:
+        print("{} argument:".format(count))
+    else:
+        print("{} arguments:".format(count))
+    for i in range(1, count + 1):
+        print("{}: {}".format(i, sys.argv[i]))
