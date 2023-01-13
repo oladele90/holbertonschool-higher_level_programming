b41ad778101d8d0409687687426fcf11b5e121e2
oladele90 <oladelemiro07@gmail.com>
2023-01-13 09:23:54 -0800

    no c

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -4,7 +4,7 @@ def no_c(my_string):
         new_string = ""
         for i in my_string:
             if i == 'c' or i == 'C':
-                new_string+= ''
-            else :
-                new_string+= i
+                new_string += ''
+            else:
+                new_string += i
         return new_string
