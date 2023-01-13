0ee1386f0d1b12da27c38ff2ca481339d37ffa79
oladele90 <oladelemiro07@gmail.com>
2023-01-13 14:41:57 -0800

    add tuple

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -5,6 +5,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     for i in range(2):
         list_a.append(0)
         list_b.append(0)
-    list_t= [list_a[x] + list_b[x] for x in range(2)]
+    list_t = [list_a[x] + list_b[x] for x in range(2)]
     new_tuple = tuple(list_t)
     return new_tuple
