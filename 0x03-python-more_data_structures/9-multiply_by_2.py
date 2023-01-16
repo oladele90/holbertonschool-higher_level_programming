b96843d532fb3ba6b2b4259543f7edd31cedf70f
oladele90 <oladelemiro07@gmail.com>
2023-01-16 09:54:48 -0800

    times 2

--- 0x03-python-more_data_structures/9-multiply_by_2.py
+++ 0x03-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new = dict(a_dictionary)
     for i in a_dictionary:
-        a_dictionary[i] = a_dictionary[i] * 2
-    return a_dictionary
+        new[i] = a_dictionary[i] * 2
+    return new
