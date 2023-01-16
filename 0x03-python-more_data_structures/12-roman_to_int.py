05de1d062ee152f15770dc78ec3c89d18ed8ffcb
oladele90 <oladelemiro07@gmail.com>
2023-01-16 11:02:14 -0800

    roman 2 int

---
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    my_dict  = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+    sum = 0
+    if not roman_string or roman_string == None:
+        return 0
+    for i in roman_string:
+        if i in my_dict:
+            sum += my_dict[i]
+    return sum
