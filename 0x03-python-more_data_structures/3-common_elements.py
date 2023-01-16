3372b4df39975faa403ef6f58032f649b2ddcfeb
oladele90 <oladelemiro07@gmail.com>
2023-01-16 08:19:32 -0800

    same elem

---
+++ 0x03-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    new = []
+    for i in set_1:
+        if i not in set_2:
+            pass
+        else:
+            new.append(i)
+    set(new)
+    return new
