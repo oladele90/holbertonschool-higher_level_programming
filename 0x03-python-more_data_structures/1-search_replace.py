d71465465213c913461062fc4fad7f2dc5da6615
oladele90 <oladelemiro07@gmail.com>
2023-01-16 07:31:36 -0800

    search and replace

--- 0x03-python-more_data_structures/1-search_replace.py
+++ 0x03-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    new = my_list.copy()
     i = -1
     try:
         while True:
             i = my_list.index(search, i + 1)
-            my_list[i] = replace
+            new[i] = replace
     except ValueError:
         pass
+    return new
