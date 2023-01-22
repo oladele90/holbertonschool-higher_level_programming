4370807971a260fc29f8e7d3786c035c154c776e
oladele90 <oladelemiro07@gmail.com>
2023-01-22 09:14:40 -0800

    pycodestyle correction

--- 0x07-python-test_drivin_development/0-add_integer.py
+++ 0x07-python-test_drivin_development/0-add_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """add_integer module"""
 
+
 def add_integer(a, b=98):
     """Adds two integers"""
+
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
