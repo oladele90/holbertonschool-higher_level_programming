6d90757d1ed8835c79cdaa92d81e6cb99705c3b6
oladele90 <oladelemiro07@gmail.com>
2023-01-27 10:53:01 -0800

    base geo

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """base_geometry module"""
 
+
 class BaseGeometry():
-    """creates empty class called BaseGeometry"""
+    """creates class called BaseGeometry"""
 
 def area(self):
     """area function"""
-    raise Exception("area() is not implemented")
+    raise Exception("is not implemented")
