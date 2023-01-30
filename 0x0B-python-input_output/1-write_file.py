23925f71b55dcbc963051f8f6d7d9ed696c0a533
oladele90 <oladelemiro07@gmail.com>
2023-01-30 09:03:16 -0800

    add

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """writes a string to a text file"""
 
-    with open(filename, 'w' encoding="utf+8") as f:
-        return f.write(text)
+    with open(filename, 'w', encoding="utf+8") as f:
+        return f.write(text, end="")
