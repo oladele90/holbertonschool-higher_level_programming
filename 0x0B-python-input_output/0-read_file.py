0392e7eb36d93690fe2a6324795add0dc555db60
oladele90 <oladelemiro07@gmail.com>
2023-01-30 08:36:35 -0800

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     """read file opens a file and prints to STDOUT"""
 
     with open(filename, encoding="utf+8") as f:
-        print(f.read())
+        print(f.read(), end="")
         f.close()
