3776053c7180b35b24ef7305303de50546b45295
oladele90 <oladelemiro07@gmail.com>
2023-01-31 08:44:02 -0800

    student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""student module"""
+
+
+class Student:
+    """create class called student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes new student"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves dictionary"""
+
+        if attrs is None or type(attrs) is not list:
+            return self.__dict__
+        else:
+            dict1 = {}
+            for i in attrs:
+                if type(i) != str:
+                    return self.__dict__
+                if i in self.__dict__.keys():
+                    dict1[i] = self.__dict__[i]
+            return dict1
