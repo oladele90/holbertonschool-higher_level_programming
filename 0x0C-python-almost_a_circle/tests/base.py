5ae062cdb9588936084edd25e0c305225b44f147
oladele90 <oladelemiro07@gmail.com>
2023-01-31 09:40:52 -0800

    test

---
+++ 0x0C-python-almost_a_circle/tests/base.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""tests for base.py"""
+import unittest
+from models.base import Base
+
+
+class Test(unittest.TestCase):
+    """unit tests"""
+
+if __name__ == '__main__':
+    unittest.main()
