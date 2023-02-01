c348f85922ee9dc21a1c278a20a0e936f2298fdc
oladele90 <oladelemiro07@gmail.com>
2023-02-01 09:44:18 -0800

    tests/

---
+++ 0x0C-python-almost_a_circle/tests/test_models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""tests for base.py"""
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+class Test(unittest.TestCase):
+    """unit tests"""
+
+    def test_non(self):
+        base1 = Base(None)
+        base2 = Base()
+        self.assertEqual(base1.id + 1, base2.id)
+
+if __name__ == '__main__':
+    unittest.main()
