96e99b26da1f978e646a4307a5d281df94531cdf
oladele90 <oladelemiro07@gmail.com>
2023-02-11 12:49:42 -0800

    15-groups.sql

--- 0x00-SQL_introduction/15-groups.sql
+++ 0x00-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- 15-groups.sql
 -- lists the number of records with the same score in the table
-SELECT DISTINCT score, COUNT(score) AS number FROM second_table GROUP BY score;
+SELECT DISTINCT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
