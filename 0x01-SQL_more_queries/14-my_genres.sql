5c42106beeb94853379cae727f2fc3e23e9b8446
oladele90 <oladelemiro07@gmail.com>
2023-02-15 09:21:26 -0800

    14

--- 0x01-SQL_more_queries/14-my_genres.sql
+++ 0x01-SQL_more_queries/14-my_genres.sql
@@ -2,5 +2,7 @@
 -- list all genres
 SELECT tv_genres.name
 FROM tv_genres INNER JOIN tv_show_genres
+INNER JOIN tv_shows
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE (tv_show_genres.show_id = 8)
+WHERE tv_shows.id = tv_show_genres.show_id
+AND tv_shows.title = 'Dexter'
