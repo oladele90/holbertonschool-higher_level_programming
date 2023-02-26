fdf5407626dff515062c4ce0057c6934f8cf4053
oladele90 <oladelemiro07@gmail.com>
2023-02-26 10:44:29 -0800

    1-filter_states.py

---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""0-select-states.py"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        for col in row:
+            if type(col) is not int:
+                if col.startswith("N"):
+                    print(row)
+    cur.close()
+    db.close()
