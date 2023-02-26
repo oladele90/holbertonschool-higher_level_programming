c1c8b8424074deb063005119f6c80524964f12aa
oladele90 <oladelemiro07@gmail.com>
2023-02-26 09:30:42 -0800

    0-select_states.

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -3,7 +3,7 @@
 import MySQLdb
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(host="localhost", user="root",
+    db = MySQLdb.connect(host="localhost", user="root", port=3306,
                          passwd="root", db="hbtn_0e_0_usa")
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
