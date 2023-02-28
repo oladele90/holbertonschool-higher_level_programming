345510f8a871f7d0d5edbc6987b1bc929062c256
oladele90 <oladelemiro07@gmail.com>
2023-02-27 20:31:11 -0800

    10-model_state_my_get.py

---
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""10-model_state_my_get.py"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.
+                           argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    results = session.query(State.id, State.name).filter(
+                            State.name == sys.argv[4]).first()
+    if results:
+        print("{}".format(results.id))
+    else:
+        print("Not Found")
