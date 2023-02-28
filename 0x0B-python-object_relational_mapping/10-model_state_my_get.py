8a1a78e24230c125fce586d051a008d53cc4fa3d
oladele90 <oladelemiro07@gmail.com>
2023-02-27 21:11:34 -0800

    10-model_state_my_get.py

--- 0x0B-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -13,9 +13,9 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    results = session.query(State.id, State.name).filter(
-                            State.name == "{}".format(sys.argv[4])).first()
+    results = session.query(State.id, State.name).filter_by(
+                            name=sys.argv[4]).first()
     if results and results is not None:
         print("{}".format(results.id))
     else:
-        print("Not Found")
+        print("Not found")
