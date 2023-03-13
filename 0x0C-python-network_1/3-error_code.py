b07bbf4e8cc9a19a49c8c7af0300063aba281da1
oladele90 <oladelemiro07@gmail.com>
2023-03-13 11:26:38 -0700

    3-error_code.py

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -5,9 +5,13 @@
 import urllib
 import urllib.request
 import urllib.parse
+import urllib.error
 import sys
 
 if __name__ == "__main__":
     req = urllib.request.Request(sys.argv[1])
-    with urllib.request.urlopen(req) as resp:
-        print(sys.argv[1])
+    try:
+        with urllib.request.urlopen(req) as resp:
+            print(resp.read().decode('utf8'))
+    except urllib.error.HTTPError as ec:
+        print("Error code: {}".format(ec))
