825a11e9b6893eacbc07440e727f40404f107fc2
oladele90 <oladelemiro07@gmail.com>
2023-03-13 11:40:08 -0700

    4-hbtn_status.py

---
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""status.py"""
+
+
+import requests
+
+if __name__ == "__main__":
+    html = requests.get("https://intranet.hbtn.io/status")
+    print("""Body response:
+\t- type: {}
+\t- content: {}
+""".format(
+              type(html.text),
+              html.text
+              ))
