5a1ab12b15975fdeab9b9a199c14170ce59ae979
oladele90 <oladelemiro07@gmail.com>
2023-03-13 11:54:07 -0700

    6-post_email.py

--- 0x0C-python-network_1/6-post_email.py
+++ 0x0C-python-network_1/6-post_email.py
@@ -6,4 +6,4 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    print(requests.post(sys.argv[1], data={'email': sys.argv[2]}.text))
+    print(requests.post(sys.argv[1], data={'email': sys.argv[2]}).text)
