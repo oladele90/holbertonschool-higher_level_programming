2bdcaf74a8395e005fb587561d02660184c46ac0
oladele90 <oladelemiro07@gmail.com>
2023-03-13 09:14:06 -0700

    0-hbtn_status.py

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -6,5 +6,12 @@ import urllib.request
 
 if __name__ == "__main__":
     req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        print(response.read())
+    with urllib.request.urlopen(req) as resp:
+        print("""Body response:
+\t- type: {}
+\t- content: {}
+\t- utf8 content: {}""".format(
+              type(resp.read()),
+              resp.read(),
+              resp.read().decode("utf8")))
+        
