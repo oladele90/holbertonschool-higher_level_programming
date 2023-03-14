d7f44b45445e4c3a183e5990628eb7d5345eb8c6
oladele90 <oladelemiro07@gmail.com>
2023-03-13 17:45:04 -0700

    7-error_code.py

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -6,8 +6,7 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    try:
-        print(requests.get(sys.argv[1]))
-        
-    except:
-        print("Error code: ".format(requests.get(sys.argv[1]).status_code))
+    if requests.get(sys.argv[1]).status_code >= 400:
+        print("Error code: {}".format(requests.get(sys.argv[1]).status_code))
+    else:
+        print(requests.get(sys.argv[1]).text)
