672209f5a0c8c81490b3f441c16d5ec280e74467
oladele90 <oladelemiro07@gmail.com>
2023-03-13 20:07:09 -0700

    8-json_api.py

---
+++ 0x0C-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""header.py"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        value = {'q': sys.argv[1]}
+    if len(sys.argv) < 2:
+        value = {'q': ""}
+    req = requests.post("http://0.0.0.0:5000/search_user", value)
+    try:
+        val = req.json()
+        if val.getid is None:
+            print("No result")
+        else:
+            print("[{}] {}".format(val.get('id'), val.get('name')))
+    except Exception:
+        print("Not a valid JSON")
