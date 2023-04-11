bc58e6beb4431efe60f64fee483b45aab098e9a6
oladele90 <oladelemiro07@gmail.com>
2023-04-11 12:06:26 -0700

    adds print c method to Square class

--- 0x01-javascript_objects_scopes_closures/6-square.js
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+const Square1 = require('./5-square');
+module.exports = class Square extends Square1 {
   charPrint (c) {
     if (c) {
       let listy = '';
