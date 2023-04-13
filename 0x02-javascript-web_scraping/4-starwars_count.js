e9b15e171b6e1ac02e04c5cca2f2dc128706166a
oladele90 <oladelemiro07@gmail.com>
2023-04-13 16:31:50 -0700

    parse starwars api for character count

---
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+request.get(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const r = JSON.parse(body);
+    let count = 0;
+    for (let i = 0; i < r.count; i++) {
+      if ([...r.results[i].characters].includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        count++;
+      }
+    }
+    console.log(count);
+  }
+});
